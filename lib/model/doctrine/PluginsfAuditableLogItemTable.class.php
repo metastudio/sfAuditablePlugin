148421ddf041bc01dda60277b6e9a2e8b30548aa
Andrey Chernih <andrey.chernih@gmail.com>
2010-05-29 14:51:50 +0400

    Fetch log items by date

--- lib/model/doctrine/PluginsfAuditableLogItemTable.class.php
+++ lib/model/doctrine/PluginsfAuditableLogItemTable.class.php
@@ -3,5 +3,23 @@
  */
 class PluginsfAuditableLogItemTable extends Doctrine_Table
 {
-
+	public function fetchForIssuer($issuerID, $groupByDate = true)
+	{
+		$logItems = Doctrine_Query::create()
+			->select("DATE_FORMAT(li.created_at, '%Y-%m-%d') AS date, DATE_FORMAT(li.created_at, '%H:%i') AS time, li.text")
+			->from("sfAuditableLogItem li")
+			->where("li.issuer_id = ? OR li.issuer_id = 0", $issuerID)
+			->orderBy("li.created_at DESC")->execute();
+		if ($groupByDate)
+		{
+			foreach($logItems as $logItem)
+			{
+				if (!isset($byDate[$logItem->date])) { $byDate[$logItem->date] = array(); }
+				$byDate[$logItem->date][] = $logItem;
+			}
+			return $byDate;
+		} else {
+			return $logItems;
+		}
+	}
 }
